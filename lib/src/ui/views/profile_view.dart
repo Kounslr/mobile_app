0d0c4707f0ec3758a98408a06d71fe04b290ba7f
31Carlton7 <carltonaikins7@gmail.com>
2021-05-27 21:12:36 -0400

    Fix sign in/sign up glitch

--- lib/src/ui/views/profile_view.dart
+++ lib/src/ui/views/profile_view.dart
@@ -1,21 +1,22 @@
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kounslr/src/ui/providers/authentication_service_provider.dart';
+import 'package:kounslr/src/ui/providers/authentication_providers/authentication_service_provider.dart';
 
 class ProfileView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
-    return _content(context);
+    return _content(context, watch);
   }
 
-  Widget _content(BuildContext context) {
+  Widget _content(BuildContext context, ScopedReader watch) {
+    final _auth = watch(authenticationServiceProvider);
     return Center(
       child: CantonPrimaryButton(
         buttonText: 'Sign out',
         textColor: CantonColors.white,
         containerColor: Theme.of(context).primaryColor,
         onPressed: () {
-          context.read(authenticationServiceProvider).signOut();
+          _auth.signOut();
         },
       ),
     );
