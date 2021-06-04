50bc9d05dd0b8defc3b6c6f88ffd300afebdad0d
31Carlton7 <carltonaikins7@gmail.com>
2021-06-04 15:44:55 -0400

    Add new function

--- lib/src/services/repositories/student_repository.dart
+++ lib/src/services/repositories/student_repository.dart
@@ -1,23 +1,57 @@
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:async';
 
+import 'package:kounslr/src/models/journal_entry.dart';
+import 'package:kounslr/src/models/journal_entry_tag.dart';
+
 class StudentRepository extends ChangeNotifier {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final CollectionReference user = FirebaseFirestore.instance
+      .collection('customers')
+      .doc('lcps')
+      .collection('schools')
+      .doc('independence')
+      .collection('students');
 
   Stream<DocumentSnapshot<Map<String, dynamic>>> getStudent(
       String district, String school, String uid) {
     try {
-      return _firestore
-          .collection('customers')
-          .doc(district)
-          .collection('schools')
-          .doc(school)
-          .collection('students')
-          .doc(uid)
-          .snapshots();
+      return user.doc(uid).snapshots();
     } catch (e) {
       rethrow;
     }
+  }
+
+  Future<void> addJournalEntry(JournalEntry entry) async {
+    String id = user
+        .doc(FirebaseAuth.instance.currentUser.uid)
+        .collection('journal entries')
+        .doc()
+        .id;
+    await user
+        .doc(FirebaseAuth.instance.currentUser.uid)
+        .collection('journal entries')
+        .doc()
+        .set(entry.copyWith(id: id).toMap());
+  }
+
+  Future<void> updateJournalEntry({
+    JournalEntry entry,
+    String title,
+    String summary,
+    List<JournalEntryTag> tags,
+  }) async {
+    await user
+        .doc(FirebaseAuth.instance.currentUser.uid)
+        .collection('journal entries')
+        .doc(entry.id)
+        .update(entry
+            .copyWith(
+                title: title,
+                summary: summary,
+                tags: tags,
+                lastEditDate: DateTime.now())
+            .toMap());
   }
 }
