0a6efbb510aed795b019138a1ca4c33bdff063ea
31Carlton7 <carltonaikins7@gmail.com>
2021-06-18 23:05:57 -0400

    Update home_view.dart

--- lib/src/ui/views/home_view.dart
+++ lib/src/ui/views/home_view.dart
@@ -204,108 +204,128 @@ class _HomeViewState extends State<HomeView> {
   }
 
   Widget _nextClassCard(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Text('Next Class', style: Theme.of(context).textTheme.headline6),
-            const Spacer(),
-            TextButton(
-              style: ButtonStyle(
-                alignment: Alignment.centerRight,
-                animationDuration: Duration.zero,
-                elevation: MaterialStateProperty.all<double>(0),
-                overlayColor: MaterialStateProperty.all<Color>(
-                  CantonColors.transparent,
+    if (![DateTime.saturday, DateTime.sunday].contains(DateTime.now().day)) {
+      return Column(
+        children: [
+          Row(
+            children: [
+              Text('Next Class', style: Theme.of(context).textTheme.headline6),
+              const Spacer(),
+              TextButton(
+                style: ButtonStyle(
+                  alignment: Alignment.centerRight,
+                  animationDuration: Duration.zero,
+                  elevation: MaterialStateProperty.all<double>(0),
+                  overlayColor: MaterialStateProperty.all<Color>(
+                    CantonColors.transparent,
+                  ),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    EdgeInsets.zero,
+                  ),
                 ),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                  EdgeInsets.zero,
+                child: Text(
+                  'View Full Schedule',
+                  style: Theme.of(context).textTheme.bodyText1.copyWith(
+                        fontWeight: FontWeight.w500,
+                        color: Theme.of(context).primaryColor,
+                      ),
                 ),
+                onPressed: () {
+                  CantonMethods.viewTransition(context, ScheduleView());
+                },
               ),
-              child: Text(
-                'View Full Schedule',
-                style: Theme.of(context).textTheme.bodyText1.copyWith(
-                      fontWeight: FontWeight.w500,
-                      color: Theme.of(context).primaryColor,
-                    ),
-              ),
-              onPressed: () {
-                CantonMethods.viewTransition(context, ScheduleView());
-              },
-            ),
-            CantonActionButton(
-              icon: IconlyIcon(
-                IconlyBold.ArrowRight2,
-                color: Theme.of(context).primaryColor,
-              ),
-              onPressed: () {
-                CantonMethods.viewTransition(context, ScheduleView());
-              },
-            ),
-          ],
-        ),
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'CLASS',
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      children: [
-                        IconlyIcon(
-                          IconlyBold.Location,
-                          color: Theme.of(context).colorScheme.secondaryVariant,
-                          size: 17,
-                        ),
-                        const SizedBox(width: 7),
-                        Text(
-                          'LOCATION',
-                          style: Theme.of(context).textTheme.bodyText1.copyWith(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .secondaryVariant,
-                              ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        IconlyIcon(
-                          IconlyBold.Profile,
-                          color: Theme.of(context).colorScheme.secondaryVariant,
-                          size: 17,
-                        ),
-                        const SizedBox(width: 7),
-                        Text(
-                          'TEACHER',
-                          style: Theme.of(context).textTheme.bodyText1.copyWith(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .secondaryVariant,
-                              ),
-                        ),
-                      ],
-                    ),
-                  ],
+              CantonActionButton(
+                icon: IconlyIcon(
+                  IconlyBold.ArrowRight2,
+                  color: Theme.of(context).primaryColor,
                 ),
-                Spacer(),
-                Text(
-                  'TIME',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ],
+                onPressed: () {
+                  CantonMethods.viewTransition(context, ScheduleView());
+                },
+              ),
+            ],
+          ),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'CLASS',
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          IconlyIcon(
+                            IconlyBold.Location,
+                            color:
+                                Theme.of(context).colorScheme.secondaryVariant,
+                            size: 17,
+                          ),
+                          const SizedBox(width: 7),
+                          Text(
+                            'LOCATION',
+                            style:
+                                Theme.of(context).textTheme.bodyText1.copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondaryVariant,
+                                    ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          IconlyIcon(
+                            IconlyBold.Profile,
+                            color:
+                                Theme.of(context).colorScheme.secondaryVariant,
+                            size: 17,
+                          ),
+                          const SizedBox(width: 7),
+                          Text(
+                            'TEACHER',
+                            style:
+                                Theme.of(context).textTheme.bodyText1.copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondaryVariant,
+                                    ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Text(
+                    'TIME',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ],
+              ),
             ),
           ),
+        ],
+      );
+    } else {
+      return Card(
+        margin: EdgeInsets.only(top: 15),
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Enjoy your weekend!',
+                  style: Theme.of(context).textTheme.headline4),
+            ],
+          ),
         ),
-      ],
-    );
+      );
+    }
   }
 }
