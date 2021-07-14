9b790ee5666c77d7497e5ffd3c81ef91783fdacf
31Carlton7 <carltonaikins7@gmail.com>
2021-07-14 18:58:26 -0400

    Rename student_provider.dart

--- lib/src/ui/providers/student_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kounslr/src/services/repositories/student_repository.dart';
-
-final studentProvider = Provider<StudentRepository>((ref) {
-  return StudentRepository();
-});
