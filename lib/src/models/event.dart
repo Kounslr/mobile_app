2269ebbe81738725b8717b0b581b45010566ddf8
31Carlton7 <carltonaikins7@gmail.com>
2021-07-14 18:54:46 -0400

    Update event model

--- lib/src/models/event.dart
+++ lib/src/models/event.dart
@@ -59,3 +59,75 @@ class Event {
   @override
   int get hashCode => name.hashCode ^ time.hashCode;
 }
+
+class EventM {
+  String? name;
+  DateTime? time;
+
+  EventM({
+    this.name,
+    this.time,
+  });
+
+  EventM copyWith({
+    String? name,
+    DateTime? time,
+  }) {
+    return EventM(
+      name: name ?? this.name,
+      time: time ?? this.time,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'time': time?.millisecondsSinceEpoch,
+    };
+  }
+
+  Map<String, dynamic> toDocumentSnapshot() {
+    return {
+      'name': name,
+      'time': Timestamp.fromDate(time!),
+    };
+  }
+
+  factory EventM.fromMap(Map<String, dynamic> map) {
+    return EventM(
+      name: map['name'],
+      time: DateTime.fromMillisecondsSinceEpoch(map['time']),
+    );
+  }
+
+  factory EventM.fromMapFromDocumentSnapshot(Map<String, dynamic> map) {
+    return EventM(
+      name: map['name'],
+      time: map['time'].toDate(),
+    );
+  }
+
+  factory EventM.fromDocumentSnapshot(DocumentSnapshot doc) {
+    return EventM(
+      name: doc['name'],
+      time: doc['time'].toDate(),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory EventM.fromJson(String source) => EventM.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'EventM(name: $name, time: $time)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is EventM && other.name == name && other.time == time;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ time.hashCode;
+}
