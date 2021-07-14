1aa69410e39d21602a8b74dcfc6a69068a15f792
31Carlton7 <carltonaikins7@gmail.com>
2021-07-14 18:57:54 -0400

    Create student_assignments_provider.dart

---
+++ lib/src/ui/providers/student_assignments_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kounslr/src/models/assignment.dart';
+import 'package:kounslr/src/services/repositories/student_repository.dart';
+
+final upcomingAssignmentsFutureProvider =
+    FutureProvider<List<AssignmentM>>((ref) {
+  return StudentRepository().upcomingAssignments;
+});
