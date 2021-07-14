dd694bac780b520e437ab803064eee5b4aaaf246
31Carlton7 <carltonaikins7@gmail.com>
2021-07-14 18:54:40 -0400

    Update current_day model

--- lib/src/models/current_day.dart
+++ lib/src/models/current_day.dart
@@ -100,3 +100,132 @@ class CurrentDay {
         events.hashCode;
   }
 }
+
+class CurrentDayM {
+  String? id;
+  String? dayType; // Could be A-Day, B-Day etc.
+  String? markingPeriod;
+  DateTime? date;
+  List<BlockM>? blocks; // Blocks 1 - 8, 1 - 4 etc.
+  List<EventM>? events; // Pep rally, Sports practice, lunch
+
+  CurrentDayM({
+    this.id,
+    this.dayType,
+    this.markingPeriod,
+    this.date,
+    this.blocks,
+    this.events,
+  });
+
+  CurrentDayM copyWith({
+    String? id,
+    String? dayType,
+    String? markingPeriod,
+    DateTime? date,
+    List<BlockM>? blocks,
+    List<EventM>? events,
+  }) {
+    return CurrentDayM(
+      id: id ?? this.id,
+      dayType: dayType ?? this.dayType,
+      markingPeriod: markingPeriod ?? this.markingPeriod,
+      date: date ?? this.date,
+      blocks: blocks ?? this.blocks,
+      events: events ?? this.events,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'dayType': dayType,
+      'markingPeriod': markingPeriod,
+      'date': date?.millisecondsSinceEpoch,
+      'blocks': blocks?.map((x) => x.toMap()).toList(),
+      'events': events?.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  Map<String, dynamic> toDocumentSnapshot() {
+    return {
+      'id': id,
+      'dayType': dayType,
+      'markingPeriod': markingPeriod,
+      'date': Timestamp.fromDate(date!),
+      'blocks': blocks?.map((x) => x.toDocumentSnapshot()).toList(),
+      'events': events?.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory CurrentDayM.fromMap(Map<String, dynamic> map) {
+    return CurrentDayM(
+      id: map['id'],
+      dayType: map['dayType'],
+      markingPeriod: map['markingPeriod'],
+      date: DateTime.fromMillisecondsSinceEpoch(map['date']),
+      blocks: List<BlockM>.from(map['blocks']?.map((x) => BlockM.fromMap(x))),
+      events: List<EventM>.from(map['events']?.map((x) => EventM.fromMap(x))),
+    );
+  }
+
+  factory CurrentDayM.fromMapFromDocumentSnapshot(Map<String, dynamic> map) {
+    return CurrentDayM(
+      id: map['id'],
+      dayType: map['dayType'],
+      markingPeriod: map['markingPeriod'],
+      date: map['date'].toDate(),
+      blocks: List<BlockM>.from(
+          map['blocks']?.map((x) => BlockM.fromMapFromDocumentSnapshot(x))),
+      events: List<EventM>.from(
+          map['events']?.map((x) => EventM.fromMapFromDocumentSnapshot(x))),
+    );
+  }
+
+  factory CurrentDayM.fromDocumentSnapshot(DocumentSnapshot doc) {
+    print(doc);
+    return CurrentDayM(
+      id: doc['id'],
+      dayType: doc['dayType'],
+      markingPeriod: doc['markingPeriod'],
+      date: doc['date'].toDate(),
+      blocks: List<BlockM>.from(
+          doc['blocks']?.map((x) => BlockM.fromDocumentSnapshot(x))),
+      events: List<EventM>.from(
+          doc['events']?.map((x) => EventM.fromDocumentSnapshot(x))),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CurrentDayM.fromJson(String source) =>
+      CurrentDayM.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'CurrentDayM(id: $id, dayType: $dayType, markingPeriod: $markingPeriod, date: $date, blocks: $blocks, events: $events)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is CurrentDayM &&
+        other.id == id &&
+        other.dayType == dayType &&
+        other.markingPeriod == markingPeriod &&
+        other.date == date &&
+        listEquals(other.blocks, blocks) &&
+        listEquals(other.events, events);
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        dayType.hashCode ^
+        markingPeriod.hashCode ^
+        date.hashCode ^
+        blocks.hashCode ^
+        events.hashCode;
+  }
+}
