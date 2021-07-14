1160299d5543722b75eadb875b3925d4cf518133
31Carlton7 <carltonaikins7@gmail.com>
2021-07-14 19:20:11 -0400

    Update assignment_card to work with models

--- lib/src/ui/styled_components/assignment_card.dart
+++ lib/src/ui/styled_components/assignment_card.dart
@@ -1,10 +1,12 @@
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:intl/intl.dart';
 import 'package:kounslr/src/models/assignment.dart';
+import 'package:kounslr/src/models/class.dart';
 
 class AssignmentCard extends StatelessWidget {
-  final Assignment? assignment;
-  const AssignmentCard(this.assignment);
+  final ClassM? schoolClass;
+  final AssignmentM? assignment;
+  const AssignmentCard(this.schoolClass, this.assignment);
 
   @override
   Widget build(BuildContext context) {
@@ -43,14 +45,14 @@ class AssignmentCard extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        assignment?.schoolClass?.className ?? 'CLASS NAME',
+                        schoolClass?.name ?? 'CLASS NAME',
                         style: Theme.of(context).textTheme.bodyText1?.copyWith(
                               color: Theme.of(context).primaryColor,
                             ),
                       ),
                       SizedBox(height: 7),
                       Text(
-                        _assignmentName(assignment?.assignmentName),
+                        _assignmentName(assignment?.name),
                         style: Theme.of(context).textTheme.headline5,
                       ),
                       SizedBox(height: 7),
