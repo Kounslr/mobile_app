5fa3cefdbef3bf8dd0b12407e90b0418165369c7
31Carlton7 <carltonaikins7@gmail.com>
2021-07-14 19:21:05 -0400

    Update imports

--- lib/src/ui/styled_components/journal_entry_tag_card.dart
+++ lib/src/ui/styled_components/journal_entry_tag_card.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:kounslr/src/models/journal_entry.dart';
-import 'package:kounslr/src/ui/providers/student_provider.dart';
+import 'package:kounslr/src/ui/providers/student_repository_provider.dart';
 import 'package:kounslr/src/ui/styled_components/journal_entry_card.dart';
 
 class JournalEntryTagCard extends StatefulWidget {
@@ -118,7 +118,7 @@ class _JournalEntryTagCardState extends State<JournalEntryTagCard> {
                     SizedBox(height: 15),
                     GestureDetector(
                       onTap: () {
-                        watch(studentProvider).renameJournalEntryTags(
+                        watch(studentRepositoryProvider).renameJournalEntryTags(
                           widget.entries,
                           widget.tag,
                           Tag(name: _tagRenameController.text),
