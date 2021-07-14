a0fbf56de5084334051a166ebe0dc0beead8b73d
31Carlton7 <carltonaikins7@gmail.com>
2021-07-14 19:22:48 -0400

    Reduce logic in schedule_view.dart

--- lib/src/ui/views/schedule_view.dart
+++ lib/src/ui/views/schedule_view.dart
@@ -1,9 +1,8 @@
 import 'package:canton_design_system/canton_design_system.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kounslr/src/models/class.dart';
-import 'package:kounslr/src/ui/providers/student_provider.dart';
+import 'package:kounslr/src/ui/providers/student_classes_stream_provider.dart';
 import 'package:kounslr/src/ui/styled_components/class_card.dart';
+import 'package:kounslr/src/ui/styled_components/something_went_wrong.dart';
 
 class ScheduleView extends StatelessWidget {
   const ScheduleView();
@@ -34,33 +33,29 @@ class ScheduleView extends StatelessWidget {
   }
 
   Widget _body(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-      stream: context.read(studentProvider).getStudentClasses(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          return Expanded(
-            child: ListView(
-              children: _getClasses(snapshot.data!.docs),
-            ),
-          );
-        } else {
-          return Loading();
-        }
+    return Consumer(
+      builder: (context, watch, child) {
+        final studentClassesRepo = watch(studentClassesFutureProvider);
+
+        return studentClassesRepo.when(
+          data: (classes) {
+            return Expanded(
+              child: ListView.builder(
+                itemCount: classes.length,
+                itemBuilder: (context, index) {
+                  return ClassCard(
+                    schoolClass: classes[index],
+                  );
+                },
+              ),
+            );
+          },
+          loading: () => Loading(),
+          error: (e, s) {
+            return SomethingWentWrong();
+          },
+        );
       },
     );
-  }
-
-  List<Widget> _getClasses(List<QueryDocumentSnapshot> snapshots) {
-    List<ClassCard> classes = [];
-
-    for (var item in snapshots) {
-      classes.add(
-        ClassCard(
-          schoolClass: Class.fromMap(item.data() as Map<String, dynamic>),
-        ),
-      );
-    }
-
-    return classes;
   }
 }
