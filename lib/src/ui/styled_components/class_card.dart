1f89a614e61786a309d92a99e5ccad8c909a7074
31Carlton7 <carltonaikins7@gmail.com>
2021-07-14 19:20:27 -0400

    Update class_card.dart

--- lib/src/ui/styled_components/class_card.dart
+++ lib/src/ui/styled_components/class_card.dart
@@ -1,14 +1,54 @@
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kounslr/src/models/block.dart';
 import 'package:kounslr/src/models/class.dart';
+import 'package:kounslr/src/ui/providers/school_repository_provider.dart';
 
-class ClassCard extends StatelessWidget {
+class ClassCard extends StatefulWidget {
   const ClassCard({this.schoolClass});
 
   @required
-  final Class? schoolClass;
+  final ClassM? schoolClass;
+
+  @override
+  _ClassCardState createState() => _ClassCardState();
+}
+
+class _ClassCardState extends State<ClassCard> {
+  String teacherName = '';
+  BlockM currentBlock = BlockM();
+
+  @override
+  void initState() {
+    super.initState();
+    _getDatabaseInfo(
+        context: context,
+        teacherId: widget.schoolClass!.teacherId!,
+        period: widget.schoolClass!.block);
+  }
+
+  void _getDatabaseInfo({
+    BuildContext? context,
+    String? teacherId,
+    int? period,
+  }) async {
+    var teacher = await context!
+        .read(schoolRepositoryProvider)
+        .getTeacherByTeacherId(teacherId!);
+    var block =
+        await context.read(schoolRepositoryProvider).getBlockByPeriod(period!);
+
+    setState(() {
+      teacherName = teacher.name!;
+      currentBlock = block;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (teacherName == '' || currentBlock.period == null) {
+      return Loading();
+    }
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(12.0),
@@ -18,7 +58,7 @@ class ClassCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  schoolClass?.className ?? 'CLASS',
+                  widget.schoolClass?.name ?? 'CLASS',
                   style: Theme.of(context).textTheme.headline4,
                 ),
                 const SizedBox(height: 10),
@@ -31,7 +71,7 @@ class ClassCard extends StatelessWidget {
                     ),
                     const SizedBox(width: 7),
                     Text(
-                      schoolClass?.roomNumber ?? 'LOCATION',
+                      widget.schoolClass?.roomNumber ?? 'LOCATION',
                       style: Theme.of(context).textTheme.bodyText1!.copyWith(
                             color:
                                 Theme.of(context).colorScheme.secondaryVariant,
@@ -49,7 +89,7 @@ class ClassCard extends StatelessWidget {
                     ),
                     const SizedBox(width: 7),
                     Text(
-                      schoolClass?.teacher?.name ?? 'TEACHER',
+                      teacherName,
                       style: Theme.of(context).textTheme.bodyText1!.copyWith(
                             color:
                                 Theme.of(context).colorScheme.secondaryVariant,
@@ -61,12 +101,22 @@ class ClassCard extends StatelessWidget {
             ),
             Spacer(),
             Text(
-              'TIME',
+              _nextClassTime(currentBlock.time.toString()),
               style: Theme.of(context).textTheme.headline6,
             ),
           ],
         ),
       ),
     );
+  }
+
+  String _nextClassTime(String string) {
+    String time =
+        string.substring(string.indexOf(' '), string.lastIndexOf(':')).trim();
+    if (time.startsWith('0')) {
+      time = time.substring(1);
+    }
+
+    return time;
   }
 }
