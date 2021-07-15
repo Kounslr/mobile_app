8050d5633fada6201242a5ad83e6bb1888735800
31Carlton7 <carltonaikins7@gmail.com>
2021-07-15 16:03:37 -0400

    Rename block model

--- lib/src/models/block.dart
+++ lib/src/models/block.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Block {
   int? period;
-  String? time;
+  DateTime? time;
 
   Block({
     this.period,
@@ -13,67 +13,9 @@ class Block {
 
   Block copyWith({
     int? period,
-    String? time,
-  }) {
-    return Block(
-      period: period ?? this.period,
-      time: time ?? this.time,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'period': period,
-      'time': time,
-    };
-  }
-
-  factory Block.fromMap(Map<String, dynamic> map) {
-    return Block(
-      period: map['period'] as int? ?? 1,
-      time: map['time'] as String?,
-    );
-  }
-
-  factory Block.fromDocumentSnapshot(DocumentSnapshot doc) {
-    return Block(
-      period: doc['period'] as int?,
-      time: doc['time'] as String?,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Block.fromJson(String source) => Block.fromMap(json.decode(source));
-
-  @override
-  String toString() => 'Block(period: $period, time: $time)';
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Block && other.period == period && other.time == time;
-  }
-
-  @override
-  int get hashCode => period.hashCode ^ time.hashCode;
-}
-
-class BlockM {
-  int? period;
-  DateTime? time;
-
-  BlockM({
-    this.period,
-    this.time,
-  });
-
-  BlockM copyWith({
-    int? period,
     DateTime? time,
   }) {
-    return BlockM(
+    return Block(
       period: period ?? this.period,
       time: time ?? this.time,
     );
@@ -93,22 +35,22 @@ class BlockM {
     };
   }
 
-  factory BlockM.fromMap(Map<String, dynamic> map) {
-    return BlockM(
+  factory Block.fromMap(Map<String, dynamic> map) {
+    return Block(
       period: map['period'],
       time: DateTime.fromMillisecondsSinceEpoch(map['time']),
     );
   }
 
-  factory BlockM.fromMapFromDocumentSnapshot(Map<String, dynamic> map) {
-    return BlockM(
+  factory Block.fromMapFromDocumentSnapshot(Map<String, dynamic> map) {
+    return Block(
       period: map['period'],
       time: map['time'].toDate(),
     );
   }
 
-  factory BlockM.fromDocumentSnapshot(DocumentSnapshot doc) {
-    return BlockM(
+  factory Block.fromDocumentSnapshot(DocumentSnapshot doc) {
+    return Block(
       period: doc['period'],
       time: doc['time'].toDate(),
     );
@@ -116,16 +58,16 @@ class BlockM {
 
   String toJson() => json.encode(toMap());
 
-  factory BlockM.fromJson(String source) => BlockM.fromMap(json.decode(source));
+  factory Block.fromJson(String source) => Block.fromMap(json.decode(source));
 
   @override
-  String toString() => 'BlockM(period: $period, time: $time)';
+  String toString() => 'Block(period: $period, time: $time)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is BlockM && other.period == period && other.time == time;
+    return other is Block && other.period == period && other.time == time;
   }
 
   @override
