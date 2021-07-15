117e2fc68cafe3c1631b48c474d9051ad3b6c1a2
31Carlton7 <carltonaikins7@gmail.com>
2021-07-15 16:03:30 -0400

    Rename assignment model

--- lib/src/models/assignment.dart
+++ lib/src/models/assignment.dart
@@ -3,134 +3,9 @@ import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
-import 'package:kounslr/src/models/class.dart';
 import 'package:kounslr/src/models/student.dart';
 
 class Assignment {
-  String? assignmentName;
-  String? type;
-  double? earnedPoints;
-  double? possiblePoints;
-  bool? completed;
-  DateTime? creationDate;
-  DateTime? dueDate;
-  Class? schoolClass;
-
-  Assignment({
-    this.assignmentName,
-    this.type,
-    this.earnedPoints,
-    this.possiblePoints,
-    this.completed,
-    this.creationDate,
-    this.dueDate,
-    this.schoolClass,
-  });
-
-  Assignment copyWith({
-    String? assignmentName,
-    String? type,
-    double? earnedPoints,
-    double? possiblePoints,
-    bool? completed,
-    DateTime? creationDate,
-    DateTime? dueDate,
-    Class? schoolClass,
-  }) {
-    return Assignment(
-      assignmentName: assignmentName ?? this.assignmentName,
-      type: type ?? this.type,
-      earnedPoints: earnedPoints ?? this.earnedPoints,
-      possiblePoints: possiblePoints ?? this.possiblePoints,
-      completed: completed ?? this.completed,
-      creationDate: creationDate ?? this.creationDate,
-      dueDate: dueDate ?? this.dueDate,
-      schoolClass: schoolClass ?? this.schoolClass,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'assignmentName': assignmentName,
-      'type': type,
-      'earnedPoints': earnedPoints,
-      'possiblePoints': possiblePoints,
-      'completed': completed,
-      'creationDate': creationDate?.millisecondsSinceEpoch,
-      'dueDate': dueDate?.millisecondsSinceEpoch,
-      'schoolClass': schoolClass?.toMap(),
-    };
-  }
-
-  factory Assignment.fromMap(Map<String, dynamic> map) {
-    return Assignment(
-      assignmentName: map['assignmentName'],
-      type: map['type'],
-      earnedPoints: map['earnedPoints'],
-      possiblePoints: map['possiblePoints'],
-      completed: map['completed'],
-      creationDate: DateTime.fromMillisecondsSinceEpoch(
-        map['creationDate'] ?? DateTime.now().millisecondsSinceEpoch,
-      ),
-      dueDate: DateTime.fromMillisecondsSinceEpoch(map['dueDate']),
-      schoolClass: Class.fromMap(map['schoolClass']),
-    );
-  }
-
-  factory Assignment.fromDocumentSnapshot(DocumentSnapshot doc) {
-    return Assignment(
-      assignmentName: doc['assignmentName'],
-      type: doc['type'],
-      earnedPoints: doc['earnedPoints'],
-      possiblePoints: doc['possiblePoints'],
-      completed: doc['completed'],
-      creationDate: DateTime.fromMillisecondsSinceEpoch(
-        doc['creationDate'] ?? DateTime.now().millisecondsSinceEpoch,
-      ),
-      dueDate: DateTime.fromMillisecondsSinceEpoch(doc['dueDate']),
-      schoolClass: Class.fromDocumentSnapshot(doc['schoolClass']),
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Assignment.fromJson(String source) =>
-      Assignment.fromMap(json.decode(source));
-
-  @override
-  String toString() {
-    return 'Assignment(assignmentName: $assignmentName, type: $type, earnedPoints: $earnedPoints, possiblePoints: $possiblePoints, completed: $completed, creationDate: $creationDate, dueDate: $dueDate, schoolClass: $schoolClass)';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Assignment &&
-        other.assignmentName == assignmentName &&
-        other.type == type &&
-        other.earnedPoints == earnedPoints &&
-        other.possiblePoints == possiblePoints &&
-        other.completed == completed &&
-        other.creationDate == creationDate &&
-        other.dueDate == dueDate &&
-        other.schoolClass == schoolClass;
-  }
-
-  @override
-  int get hashCode {
-    return assignmentName.hashCode ^
-        type.hashCode ^
-        earnedPoints.hashCode ^
-        possiblePoints.hashCode ^
-        completed.hashCode ^
-        creationDate.hashCode ^
-        dueDate.hashCode ^
-        schoolClass.hashCode;
-  }
-}
-
-class AssignmentM {
   String? id;
   String? classId;
   String? name;
@@ -140,7 +15,7 @@ class AssignmentM {
   DateTime? dueDate;
   List<StudentInAssignment>? students;
 
-  AssignmentM({
+  Assignment({
     this.id,
     this.classId,
     this.name,
@@ -151,7 +26,7 @@ class AssignmentM {
     this.students,
   });
 
-  AssignmentM copyWith({
+  Assignment copyWith({
     String? id,
     String? classId,
     String? name,
@@ -161,7 +36,7 @@ class AssignmentM {
     DateTime? dueDate,
     List<StudentInAssignment>? students,
   }) {
-    return AssignmentM(
+    return Assignment(
       id: id ?? this.id,
       classId: classId ?? this.classId,
       name: name ?? this.name,
@@ -199,8 +74,8 @@ class AssignmentM {
     };
   }
 
-  factory AssignmentM.fromMap(Map<String, dynamic> map) {
-    return AssignmentM(
+  factory Assignment.fromMap(Map<String, dynamic> map) {
+    return Assignment(
       id: map['id'],
       classId: map['classId'],
       name: map['name'],
@@ -213,8 +88,8 @@ class AssignmentM {
     );
   }
 
-  factory AssignmentM.fromDocumentSnapshot(DocumentSnapshot doc) {
-    return AssignmentM(
+  factory Assignment.fromDocumentSnapshot(DocumentSnapshot doc) {
+    return Assignment(
       id: doc['id'],
       classId: doc['classId'],
       name: doc['name'],
@@ -229,19 +104,19 @@ class AssignmentM {
 
   String toJson() => json.encode(toMap());
 
-  factory AssignmentM.fromJson(String source) =>
-      AssignmentM.fromMap(json.decode(source));
+  factory Assignment.fromJson(String source) =>
+      Assignment.fromMap(json.decode(source));
 
   @override
   String toString() {
-    return 'AssignmentM(id: $id, classId: $classId, name: $name, type: $type, possiblePoints: $possiblePoints, creationDate: $creationDate, dueDate: $dueDate, students: $students)';
+    return 'Assignment(id: $id, classId: $classId, name: $name, type: $type, possiblePoints: $possiblePoints, creationDate: $creationDate, dueDate: $dueDate, students: $students)';
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is AssignmentM &&
+    return other is Assignment &&
         other.id == id &&
         other.classId == classId &&
         other.name == name &&
