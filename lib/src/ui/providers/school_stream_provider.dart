5ddccf972c2211b9e1efab340de1ee3530dd65fd
31Carlton7 <carltonaikins7@gmail.com>
2021-07-15 16:05:57 -0400

    Refactor provider to use new models

--- lib/src/ui/providers/school_stream_provider.dart
+++ lib/src/ui/providers/school_stream_provider.dart
@@ -2,6 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kounslr/src/models/school.dart';
 import 'package:kounslr/src/services/repositories/student_repository.dart';
 
-final schoolStreamProvider = StreamProvider<SchoolM>((ref) {
+final schoolStreamProvider = StreamProvider<School>((ref) {
   return StudentRepository().school;
 });
