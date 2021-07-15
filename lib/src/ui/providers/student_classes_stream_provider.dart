ce1309b12764a970bb6556af91a43cc75e7ea834
31Carlton7 <carltonaikins7@gmail.com>
2021-07-15 16:06:18 -0400

    Refector provider to use new models

--- lib/src/ui/providers/student_classes_stream_provider.dart
+++ lib/src/ui/providers/student_classes_stream_provider.dart
@@ -2,6 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kounslr/src/models/class.dart';
 import 'package:kounslr/src/services/repositories/student_repository.dart';
 
-final studentClassesFutureProvider = FutureProvider<List<ClassM>>((ref) {
+final studentClassesFutureProvider = FutureProvider<List<Class>>((ref) {
   return StudentRepository().studentClasses;
 });
