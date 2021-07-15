b65744ead6bf7fdc3a70b49ec6492fd8f2ed5231
31Carlton7 <carltonaikins7@gmail.com>
2021-07-15 16:04:04 -0400

    Rename event model

--- lib/src/models/event.dart
+++ lib/src/models/event.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Event {
   String? name;
-  String? time;
+  DateTime? time;
 
   Event({
     this.name,
@@ -13,67 +13,9 @@ class Event {
 
   Event copyWith({
     String? name,
-    String? time,
-  }) {
-    return Event(
-      name: name ?? this.name,
-      time: time ?? this.time,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'time': time,
-    };
-  }
-
-  factory Event.fromMap(Map<String, dynamic> map) {
-    return Event(
-      name: map['name'],
-      time: map['time'],
-    );
-  }
-
-  factory Event.fromDocumentSnapshot(DocumentSnapshot doc) {
-    return Event(
-      name: doc['name'],
-      time: doc['time'],
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Event.fromJson(String source) => Event.fromMap(json.decode(source));
-
-  @override
-  String toString() => 'Event(name: $name, time: $time)';
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Event && other.name == name && other.time == time;
-  }
-
-  @override
-  int get hashCode => name.hashCode ^ time.hashCode;
-}
-
-class EventM {
-  String? name;
-  DateTime? time;
-
-  EventM({
-    this.name,
-    this.time,
-  });
-
-  EventM copyWith({
-    String? name,
     DateTime? time,
   }) {
-    return EventM(
+    return Event(
       name: name ?? this.name,
       time: time ?? this.time,
     );
@@ -93,22 +35,22 @@ class EventM {
     };
   }
 
-  factory EventM.fromMap(Map<String, dynamic> map) {
-    return EventM(
+  factory Event.fromMap(Map<String, dynamic> map) {
+    return Event(
       name: map['name'],
       time: DateTime.fromMillisecondsSinceEpoch(map['time']),
     );
   }
 
-  factory EventM.fromMapFromDocumentSnapshot(Map<String, dynamic> map) {
-    return EventM(
+  factory Event.fromMapFromDocumentSnapshot(Map<String, dynamic> map) {
+    return Event(
       name: map['name'],
       time: map['time'].toDate(),
     );
   }
 
-  factory EventM.fromDocumentSnapshot(DocumentSnapshot doc) {
-    return EventM(
+  factory Event.fromDocumentSnapshot(DocumentSnapshot doc) {
+    return Event(
       name: doc['name'],
       time: doc['time'].toDate(),
     );
@@ -116,16 +58,16 @@ class EventM {
 
   String toJson() => json.encode(toMap());
 
-  factory EventM.fromJson(String source) => EventM.fromMap(json.decode(source));
+  factory Event.fromJson(String source) => Event.fromMap(json.decode(source));
 
   @override
-  String toString() => 'EventM(name: $name, time: $time)';
+  String toString() => 'Event(name: $name, time: $time)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is EventM && other.name == name && other.time == time;
+    return other is Event && other.name == name && other.time == time;
   }
 
   @override
