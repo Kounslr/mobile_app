bde09a0a93de81e276d8ad343e9494806f79fe68
31Carlton7 <carltonaikins7@gmail.com>
2021-07-19 14:17:38 -0400

    Add StudentVue sign in support and functionality

--- lib/src/services/authentication/authentication_service.dart
+++ lib/src/services/authentication/authentication_service.dart
@@ -2,6 +2,8 @@ import 'package:canton_design_system/canton_design_system.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:kounslr/src/models/student.dart';
+import 'package:kounslr/src/services/repositories/studentvue_repository.dart';
 
 class AuthenticationService {
   final FirebaseAuth _firebaseAuth;
@@ -14,75 +16,9 @@ class AuthenticationService {
       .doc('independence')
       .collection('students');
 
-  // String uid = '';
-
-  // Future<void> _createStudentInDatabase(
-  //   Student student,
-  //   List<Class> classes,
-  // ) async {
-  //   await user.doc(student.id).set(student.toMap());
-
-  //   classes.forEach((element) async {
-  //     await user
-  //         .doc(student.id)
-  //         .collection('classes')
-  //         .doc(element.className)
-  //         .set(element.toMap());
-  //   });
-
-  //   classes.forEach((e) async {
-  //     e.assignments!.forEach((element) async {
-  //       await user
-  //           .doc(student.id)
-  //           .collection('classes')
-  //           .doc(e.className)
-  //           .collection('assignments')
-  //           .doc(element.assignmentName)
-  //           .set(element.toMap());
-  //     });
-  //   });
-  // }
-
-  // Future<void> _updateStudentInDatabase(
-  //   Student student,
-  //   List<Class> classes,
-  // ) async {
-  //   await user.doc(student.id).set(student.toMap());
-
-  //   classes.forEach((element) async {
-  //     await user
-  //         .doc(student.id)
-  //         .collection('classes')
-  //         .doc(element.className)
-  //         .delete();
-
-  //     await user
-  //         .doc(student.id)
-  //         .collection('classes')
-  //         .doc(element.className)
-  //         .set(element.toMap());
-  //   });
-
-  //   classes.forEach((e) async {
-  //     e.assignments!.forEach((element) async {
-  //       await user
-  //           .doc(student.id)
-  //           .collection('classes')
-  //           .doc(e.className)
-  //           .collection('assignments')
-  //           .doc(element.assignmentName)
-  //           .delete();
-
-  //       await user
-  //           .doc(student.id)
-  //           .collection('classes')
-  //           .doc(e.className)
-  //           .collection('assignments')
-  //           .doc(element.assignmentName)
-  //           .set(element.toMap());
-  //     });
-  //   });
-  // }
+  Future<void> _createStudentInDatabase(Student student) async {
+    await user.doc(student.id).set(student.toMap());
+  }
 
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
@@ -93,24 +29,10 @@ class AuthenticationService {
   Future<void> signInWithEmailAndPassword(
       {required String email, required String password}) async {
     try {
-      // var student = Student();
-      // List<Class> classes = [];
-      // String username = email.substring(0, email.indexOf('@'));
-      // String domain = 'portal.lcps.org';
-
       await _firebaseAuth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-      // .then((value) => uid = value.user!.uid);
-
-      // student =
-      //     await StudentVueClient(username, password, domain).loadStudentData();
-
-      // classes =
-      //     await StudentVueClient(username, password, domain).loadGradebook();
-
-      // await _updateStudentInDatabase(student, classes);
     } on FirebaseAuthException catch (e) {
       throw e.message!;
     }
@@ -135,27 +57,36 @@ class AuthenticationService {
     }
   }
 
+  Future<String> studentVueSignIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      var student = Student();
+      String username = email.substring(0, email.indexOf('@'));
+      String domain = 'portal.lcps.org';
+      student =
+          await StudentVueClient(username, password, domain).loadStudentData();
+      student.id = FirebaseAuth.instance.currentUser?.uid;
+
+      if (student.studentId == null) return 'failed';
+
+      _createStudentInDatabase(student);
+
+      return 'success';
+    } catch (e) {
+      return 'failed';
+    }
+  }
+
   Future<void> signUp({
     required String email,
     required String password,
     String? domain,
   }) async {
     try {
-      // var student = Student();
-      // List<Class> classes = [];
-      // String username = email.substring(0, email.indexOf('@'));
-
       await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
-      // .then((value) => {uid = value.user!.uid, student.id = uid});
-
-      // student =
-      //     await StudentVueClient(username, password, domain).loadStudentData();
-
-      // classes =
-      //     await StudentVueClient(username, password, domain).loadGradebook();
-
-      // await _createStudentInDatabase(student, classes);
     } on FirebaseAuthException catch (e) {
       throw e.message!;
     }
