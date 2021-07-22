f465c6ee77c5381fe76f4708fef2cb79801f6bba
31Carlton7 <carltonaikins7@gmail.com>
2021-07-22 15:40:08 -0400

    Increase security in current_view.dart

--- lib/src/ui/views/current_view.dart
+++ lib/src/ui/views/current_view.dart
@@ -1,5 +1,7 @@
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:kounslr/src/config/bottom_navigation_bar.dart';
+import 'package:kounslr/src/ui/styled_components/something_went_wrong.dart';
 import 'package:kounslr/src/ui/views/chat_view.dart';
 import 'package:kounslr/src/ui/views/hall_pass_view.dart';
 import 'package:kounslr/src/ui/views/home_view.dart';
@@ -27,6 +29,12 @@ class _CurrentViewState extends State<CurrentView> {
 
   @override
   Widget build(BuildContext context) {
+    if (FirebaseAuth.instance.currentUser == null) {
+      return CantonScaffold(
+        body: SomethingWentWrong(),
+      );
+    }
+
     return CantonScaffold(
       bottomNavBar: BottomNavBar(_currentIndex, onTabTapped),
       body: _views[_currentIndex],
