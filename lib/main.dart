1bacf31cc47aaf6dfec7f176619a01ac3b9b6bb1
31Carlton7 <carltonaikins7@gmail.com>
2021-08-01 21:13:20 -0400

    Add Firebase Crashlytics monitoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:kounslr/src/config/constants.dart';
 import 'package:kounslr/src/services/authentication/authentication_wrapper.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
   /// Lock screen orientation to vertical
   SystemChrome.setPreferredOrientations(
