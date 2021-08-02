9cdbff3d84e754b6313e4a1fe0cd390e9df137f0
31Carlton7 <carltonaikins7@gmail.com>
2021-08-01 21:38:19 -0400

    Create no_student_data_view.dart

---
+++ lib/src/ui/views/no_student_data_view.dart
@@ -0,0 +1,50 @@
+import 'package:canton_design_system/canton_design_system.dart';
+import 'package:kounslr/src/ui/styled_components/sign_in_with_studentvue_card.dart';
+
+class NoStudentDataView extends StatelessWidget {
+  const NoStudentDataView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _content(context);
+  }
+
+  Widget _content(BuildContext context) {
+    return Column(
+      children: [
+        _header(context),
+        Expanded(
+          child: SignInWithStudentVueCard(),
+        )
+      ],
+    );
+  }
+
+  Widget _header(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hey,',
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Theme.of(context).colorScheme.secondaryVariant,
+                  ),
+            ),
+            Text(
+              'There 👋',
+              style: Theme.of(context).textTheme.headline2!.copyWith(
+                    fontWeight: FontWeight.w700,
+                  ),
+            ),
+          ],
+        ),
+        Spacer(),
+        CantonHeaderButton(
+          backgroundColor: CantonColors.transparent,
+        ),
+      ],
+    );
+  }
+}
