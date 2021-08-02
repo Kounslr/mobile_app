719af509a50af5f90059d1e8f32c3fc24d34d2ec
31Carlton7 <carltonaikins7@gmail.com>
2021-08-01 21:17:23 -0400

    Add maintainState logic for performance

--- lib/src/ui/providers/next_class_providers/next_class_stream_provider.dart
+++ lib/src/ui/providers/next_class_providers/next_class_stream_provider.dart
@@ -1,7 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kounslr/src/models/class.dart';
 import 'package:kounslr/src/services/repositories/student_repository.dart';
+import 'package:kounslr/src/ui/providers/authentication_providers/authentication_stream_provider.dart';
 
-final nextClassStreamProvider = StreamProvider<Class>((ref) {
+final nextClassStreamProvider = StreamProvider.autoDispose<Class>((ref) {
+  ref.watch(authenticationStreamProvider).whenData((value) {
+    if (value == null) {
+      ref.maintainState = false;
+    } else {
+      ref.maintainState = true;
+    }
+  });
   return StudentRepository().nextClassStream;
 });
