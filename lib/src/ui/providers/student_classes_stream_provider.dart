55d8a198c6943d174714958d96a4fc9dad2ed74a
31Carlton7 <carltonaikins7@gmail.com>
2021-08-01 21:17:11 -0400

    Update studentClassesStream

--- lib/src/ui/providers/student_classes_stream_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kounslr/src/models/class.dart';
-import 'package:kounslr/src/services/repositories/student_repository.dart';
-
-final studentClassesFutureProvider = FutureProvider<List<Class>>((ref) {
-  return StudentRepository().studentClasses;
-});
