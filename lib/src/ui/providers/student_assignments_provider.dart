d623a0bdd7750db0d519d2f5b81c075baf7cfdad
31Carlton7 <carltonaikins7@gmail.com>
2021-08-01 21:16:43 -0400

    Add maintainState logic for performance

--- lib/src/ui/providers/student_assignments_provider.dart
+++ lib/src/ui/providers/student_assignments_provider.dart
@@ -2,7 +2,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kounslr/src/models/assignment.dart';
 import 'package:kounslr/src/services/repositories/student_repository.dart';
 
-final upcomingAssignmentsFutureProvider =
-    FutureProvider<List<Assignment>>((ref) {
+import 'authentication_providers/authentication_stream_provider.dart';
+
+final upcomingAssignmentsStreamProvider =
+    StreamProvider.autoDispose<List<Assignment>>((ref) {
+  ref.watch(authenticationStreamProvider).whenData((value) {
+    if (value == null) {
+      ref.maintainState = false;
+    } else {
+      ref.maintainState = true;
+    }
+  });
   return StudentRepository().upcomingAssignments;
 });
