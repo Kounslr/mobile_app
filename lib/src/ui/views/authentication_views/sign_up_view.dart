77b90c2ffb228653acbadfdb72c009e12c24f806
31Carlton7 <carltonaikins7@gmail.com>
2021-08-01 21:23:32 -0400

    Clean up logic in sign_up_view.dart

--- lib/src/ui/views/authentication_views/sign_up_view.dart
+++ lib/src/ui/views/authentication_views/sign_up_view.dart
@@ -2,90 +2,85 @@ import 'package:canton_design_system/canton_design_system.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kounslr/src/ui/providers/authentication_providers/authentication_service_provider.dart';
 
-class SignUpView extends ConsumerWidget {
+class SignUpView extends StatefulWidget {
   final Function? toggleView;
   const SignUpView({this.toggleView});
+
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
+  _SignUpViewState createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  @override
+  Widget build(BuildContext context) {
     final _emailController = TextEditingController();
     final _passwordController = TextEditingController();
-    final _domainController = TextEditingController(text: 'portal.lcps.org');
 
     return CantonScaffold(
+      resizeToAvoidBottomInset: true,
       body: _content(
         context,
-        watch,
         _emailController,
         _passwordController,
-        _domainController,
       ),
     );
   }
 
   Widget _content(
     BuildContext context,
-    ScopedReader watch,
     TextEditingController _emailController,
     TextEditingController _passwordController,
-    TextEditingController _domainController,
   ) {
-    return Column(
-      children: [
-        _header(context),
-        SizedBox(height: 50),
-        CantonTextInput(
-          hintText: 'Email',
-          isTextFormField: true,
-          obscureText: false,
-          controller: _emailController,
-          border: BorderSide.none,
-          radius: BorderRadius.all(Radius.circular(45)),
-        ),
-        SizedBox(height: 15),
-        CantonTextInput(
-          hintText: 'Password',
-          isTextFormField: true,
-          obscureText: true,
-          controller: _passwordController,
-          border: BorderSide.none,
-          radius: BorderRadius.all(Radius.circular(45)),
-        ),
-        SizedBox(height: 15),
-        CantonTextInput(
-          hintText: 'Domain',
-          isTextFormField: true,
-          obscureText: false,
-          controller: _domainController,
-          border: BorderSide.none,
-          radius: BorderRadius.all(Radius.circular(45)),
-        ),
-        SizedBox(height: 15),
-        Row(
-          children: [
-            CantonPrimaryButton(
-              buttonText: 'Sign In',
-              containerWidth: MediaQuery.of(context).size.width / 2 - 34,
-              containerColor: Theme.of(context).colorScheme.secondary,
-              textColor: Theme.of(context).colorScheme.secondaryVariant,
-              onPressed: () => toggleView!(),
-            ),
-            Spacer(),
-            CantonPrimaryButton(
-              buttonText: 'Sign Up',
-              containerWidth: MediaQuery.of(context).size.width / 2 - 34,
-              containerColor: Theme.of(context).primaryColor,
-              textColor: CantonColors.white,
-              onPressed: () async {
-                await context.read(authenticationServiceProvider).signUp(
-                      email: _emailController.text.trim(),
-                      password: _passwordController.text.trim(),
-                      domain: _domainController.text.trim(),
-                    );
-              },
-            ),
-          ],
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          _header(context),
+          SizedBox(height: 50),
+          CantonTextInput(
+            hintText: 'Email',
+            isTextFormField: true,
+            obscureText: false,
+            controller: _emailController,
+            textInputType: TextInputType.emailAddress,
+            border: BorderSide.none,
+            radius: BorderRadius.all(Radius.circular(45)),
+          ),
+          SizedBox(height: 15),
+          CantonTextInput(
+            hintText: 'Password',
+            isTextFormField: true,
+            obscureText: true,
+            controller: _passwordController,
+            border: BorderSide.none,
+            radius: BorderRadius.all(Radius.circular(45)),
+          ),
+          SizedBox(height: 15),
+          Row(
+            children: [
+              CantonPrimaryButton(
+                buttonText: 'Sign In',
+                containerWidth: MediaQuery.of(context).size.width / 2 - 34,
+                containerColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).colorScheme.secondaryVariant,
+                onPressed: () => widget.toggleView!(),
+              ),
+              Spacer(),
+              CantonPrimaryButton(
+                buttonText: 'Sign Up',
+                containerWidth: MediaQuery.of(context).size.width / 2 - 34,
+                containerColor: Theme.of(context).primaryColor,
+                textColor: CantonColors.white,
+                onPressed: () async {
+                  await context.read(authenticationServiceProvider).signUp(
+                        email: _emailController.text.trim(),
+                        password: _passwordController.text.trim(),
+                      );
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
