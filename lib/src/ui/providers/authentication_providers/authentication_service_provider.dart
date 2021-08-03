f4862a9d730edcfb7383b0a392e08a050a1720e7
31Carlton7 <carltonaikins7@gmail.com>
2021-08-03 15:27:57 -0400

    Rename auth service provider

--- lib/src/ui/providers/authentication_providers/authentication_service_provider.dart
+++ lib/src/ui/providers/authentication_providers/authentication_service_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kounslr/src/services/authentication/authentication_service.dart';
+import 'package:kounslr/src/services/authentication/authentication_repository.dart';
 import 'package:kounslr/src/ui/providers/authentication_providers/firebaseauth_provider.dart';
 
-final authenticationServiceProvider = Provider<AuthenticationService>((ref) {
-  return AuthenticationService(ref.read(firebaseAuthProvider));
+final authenticationServiceProvider = Provider<AuthenticationRepository>((ref) {
+  return AuthenticationRepository(ref.read(firebaseAuthProvider));
 });
