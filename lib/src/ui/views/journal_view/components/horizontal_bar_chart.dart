b2d3b503d06243336d13d9215e8e1a7dded3de9e
31Carlton7 <carltonaikins7@gmail.com>
2021-08-20 16:06:57 -0400

    Update `horizontal_bar_chart.dart`

--- lib/src/ui/views/journal_view/components/horizontal_bar_chart.dart
+++ lib/src/ui/views/journal_view/components/horizontal_bar_chart.dart
@@ -10,33 +10,83 @@ class HorizontalBarChart extends StatelessWidget {
   Widget build(BuildContext context) {
     var data = tags.entries.toList();
 
-    charts.Color? seriesColor = charts.Color(
+    final charts.Color? seriesColor = charts.Color(
       r: Theme.of(context).primaryColor.red,
       g: Theme.of(context).primaryColor.green,
       b: Theme.of(context).primaryColor.blue,
     );
 
-    List<charts.Series<dynamic, String>> series = [
+    final charts.Color? labelColor = charts.Color(
+      r: Theme.of(context).colorScheme.primary.red,
+      g: Theme.of(context).colorScheme.primary.green,
+      b: Theme.of(context).colorScheme.primary.blue,
+    );
+
+    final List<charts.Series<dynamic, String>> series = [
       new charts.Series(
         id: 'Tags',
         seriesColor: seriesColor,
         domainFn: (tag, _) => tag.key,
         measureFn: (tag, _) => tag.value,
         data: data,
-      )
+      ),
     ];
 
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          'Most Used Tags',
-          style: Theme.of(context).textTheme.headline5,
+        Divider(),
+        Container(
+          color: Theme.of(context).colorScheme.secondary,
+          padding: const EdgeInsets.only(left: 17, bottom: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(left: 17, right: 17, top: 10),
+                child: Text(
+                  'Most Used Tags',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              ),
+              AspectRatio(
+                aspectRatio: 2,
+                child: charts.BarChart(
+                  series,
+                  vertical: false,
+                  secondaryMeasureAxis: charts.NumericAxisSpec(
+                    renderSpec: charts.GridlineRendererSpec(
+                      labelStyle: charts.TextStyleSpec(
+                        color: labelColor,
+                        fontSize: 12,
+                      ),
+                      lineStyle: charts.LineStyleSpec(color: labelColor),
+                    ),
+                  ),
+                  domainAxis: charts.OrdinalAxisSpec(
+                    renderSpec: charts.GridlineRendererSpec(
+                      labelStyle: charts.TextStyleSpec(
+                        color: labelColor,
+                        fontSize: 12,
+                      ),
+                      lineStyle: charts.LineStyleSpec(
+                          color: charts.MaterialPalette.transparent),
+                    ),
+                  ),
+                  primaryMeasureAxis: charts.NumericAxisSpec(
+                    renderSpec: charts.GridlineRendererSpec(
+                      labelStyle: charts.TextStyleSpec(
+                        color: labelColor,
+                        fontSize: 12,
+                      ),
+                      lineStyle: charts.LineStyleSpec(color: labelColor),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        AspectRatio(
-          aspectRatio: 2,
-          child: charts.BarChart(series, vertical: false),
-        ),
+        Divider(),
       ],
     );
   }
