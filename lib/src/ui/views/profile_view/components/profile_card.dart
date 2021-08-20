0915325fcb6da0c4aa8b9ddcf8752ab6f5da4a7c
31Carlton7 <carltonaikins7@gmail.com>
2021-08-20 16:08:20 -0400

    Create `ProfileView` components

---
+++ lib/src/ui/views/profile_view/components/profile_card.dart
@@ -0,0 +1,53 @@
+import 'package:canton_design_system/canton_design_system.dart';
+import 'package:kounslr/src/models/school.dart';
+import 'package:kounslr/src/models/student.dart';
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({required this.school, required this.student});
+
+  final Student student;
+  final School school;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  _contentText(student.name!) + ' (' + student.studentId! + ')',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Text(
+                  student.email!,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  school.name!,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ],
+            ),
+            Spacer(),
+            IconlyIcon(
+              IconlyBold.Profile,
+              size: 40,
+              color: Theme.of(context).colorScheme.secondaryVariant,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _contentText(String string) {
+    if (string.length > 23) {
+      return string.substring(0, 20) + '...';
+    }
+    return string;
+  }
+}
--- lib/src/ui/views/profile_view/profile_view.dart
+++ lib/src/ui/views/profile_view/profile_view.dart
@@ -1,18 +1,19 @@
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kounslr/src/models/school.dart';
 import 'package:kounslr/src/models/student.dart';
-import 'package:kounslr/src/ui/providers/authentication_providers/authentication_service_provider.dart';
 import 'package:kounslr/src/ui/providers/school_future_provider.dart';
+import 'package:kounslr/src/ui/providers/student_stream_provider.dart';
 import 'package:kounslr/src/ui/styled_components/something_went_wrong.dart';
-import 'package:kounslr/src/ui/views/schedule_view.dart';
-import 'package:kounslr/src/ui/views/profile_view/student_id_card_view.dart';
-import 'package:kounslr/src/ui/views/upcoming_assignments_view.dart';
+import 'package:kounslr/src/ui/views/profile_view/components/profile_card.dart';
+import 'package:kounslr/src/ui/views/profile_view/components/profile_view_header.dart';
+import 'package:kounslr/src/ui/views/profile_view/components/sign_out_button.dart';
+import 'package:kounslr/src/ui/views/profile_view/components/student_id_card.dart';
+import 'package:kounslr/src/ui/views/profile_view/components/student_schedule_card.dart';
+import 'package:kounslr/src/ui/views/profile_view/components/student_upcoming_assignments_card.dart';
 
 class ProfileView extends ConsumerWidget {
-  final Student student;
-
-  const ProfileView(this.student);
+  const ProfileView();
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
@@ -22,210 +23,47 @@ class ProfileView extends ConsumerWidget {
   Widget _content(BuildContext context, ScopedReader watch) {
     return Column(
       children: [
-        _header(context),
-        _body(context, student),
+        ProfileViewHeader(),
+        _body(context),
       ],
     );
   }
 
-  Widget _header(BuildContext context) {
-    return ViewHeaderTwo(
-      title: 'Profile',
-      backButton: true,
-      isBackButtonClear: true,
-    );
-  }
-
-  Widget _body(BuildContext context, Student student) {
+  Widget _body(BuildContext context) {
     return Consumer(
       builder: (context, watch, child) {
         final schoolRepo = watch(schoolFutureProvider);
+        final studentRepo = watch(studentStreamProvider);
+
         return schoolRepo.when(
           loading: () => Loading(),
           error: (e, s) {
             return SomethingWentWrong();
           },
           data: (school) {
-            return Column(
-              children: [
-                _profileCard(context, student, school),
-                SizedBox(height: 30),
-                _studentIDCard(context, student, school),
-                _studentScheduleCard(context),
-                _studentUpcomingAssignmentsCard(context),
-                SizedBox(height: 30),
-                GestureDetector(
-                  onTap: () {
-                    context.read(authenticationServiceProvider).signOut();
-                    Navigator.of(context).pop();
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Sign Out',
-                      style: Theme.of(context).textTheme.headline6?.copyWith(
-                            color: Theme.of(context).primaryColor,
-                          ),
-                    ),
-                  ),
-                )
-              ],
+            return studentRepo.when(
+              error: (e, s) {
+                return SomethingWentWrong();
+              },
+              loading: () => Loading(),
+              data: (student) {
+                return Column(
+                  children: [
+                    ProfileCard(school: school, student: student),
+                    const SizedBox(height: 30),
+                    StudentIdCard(school: school, student: student),
+                    StudentScheduleCard(school: school, student: student),
+                    StudentUpcomingAssignmentsCard(
+                        school: school, student: student),
+                    const SizedBox(height: 30),
+                    SignOutButton(),
+                  ],
+                );
+              },
             );
           },
         );
       },
     );
-  }
-
-  Widget _profileCard(BuildContext context, Student student, School school) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Row(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  _contentText(student.name!) + ' (' + student.studentId! + ')',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                Text(
-                  student.email!,
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Text(
-                  school.name!,
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-              ],
-            ),
-            Spacer(),
-            IconlyIcon(
-              IconlyBold.Profile,
-              size: 40,
-              color: Theme.of(context).colorScheme.secondaryVariant,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _studentIDCard(BuildContext context, Student student, School school) {
-    return GestureDetector(
-      onTap: () {
-        CantonMethods.viewTransition(
-            context, StudentIDCardView(student, school));
-      },
-      child: Card(
-        margin: EdgeInsets.zero,
-        shape: SquircleBorder(
-          radius: BorderRadius.vertical(
-            top: Radius.circular(37),
-          ),
-          side: BorderSide(
-            width: 1.5,
-            color: Theme.of(context).colorScheme.onSecondary,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Row(
-            children: [
-              Text(
-                'ID Card',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              Spacer(),
-              IconlyIcon(
-                IconlyBold.Wallet,
-                color: Theme.of(context).colorScheme.secondaryVariant,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _studentScheduleCard(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        CantonMethods.viewTransition(context, ScheduleView());
-      },
-      child: Card(
-        margin: EdgeInsets.zero,
-        shape: Border(
-          left: BorderSide(
-            width: 1.5,
-            color: Theme.of(context).colorScheme.onSecondary,
-          ),
-          right: BorderSide(
-            width: 1.5,
-            color: Theme.of(context).colorScheme.onSecondary,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Row(
-            children: [
-              Text(
-                'Schedule',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              Spacer(),
-              IconlyIcon(
-                IconlyBold.Calendar,
-                color: Theme.of(context).colorScheme.secondaryVariant,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _studentUpcomingAssignmentsCard(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        CantonMethods.viewTransition(context, UpcomingAssignmentView());
-      },
-      child: Card(
-        margin: EdgeInsets.zero,
-        shape: SquircleBorder(
-          radius: BorderRadius.vertical(
-            bottom: Radius.circular(37),
-          ),
-          side: BorderSide(
-            width: 1.5,
-            color: Theme.of(context).colorScheme.onSecondary,
-          ),
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(15),
-          child: Row(
-            children: [
-              Text(
-                'Upcoming Assignments',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              Spacer(),
-              IconlyIcon(
-                IconlyBold.Paper,
-                color: Theme.of(context).colorScheme.secondaryVariant,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  String _contentText(String string) {
-    if (string.length > 23) {
-      return string.substring(0, 20) + '...';
-    }
-    return string;
   }
 }
