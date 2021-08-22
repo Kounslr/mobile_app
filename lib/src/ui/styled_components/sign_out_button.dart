e7f6bbea3b36eddc3f6dd0e4349f07409464f193
31Carlton7 <carltonaikins7@gmail.com>
2021-08-21 23:39:07 -0400

    Create `sign_out_button.dart`

---
+++ lib/src/ui/styled_components/sign_out_button.dart
@@ -0,0 +1,21 @@
+import 'package:canton_design_system/canton_design_system.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kounslr/src/ui/providers/authentication_providers/authentication_service_provider.dart';
+
+class SignOutButton extends StatelessWidget {
+  const SignOutButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return CantonPrimaryButton(
+      buttonText: 'Sign Out',
+      textColor: Theme.of(context).primaryColor,
+      color: CantonColors.transparent,
+      alignment: MainAxisAlignment.center,
+      containerWidth: MediaQuery.of(context).size.width / 2 - 5,
+      onPressed: () {
+        context.read(authenticationServiceProvider).signOut();
+      },
+    );
+  }
+}
