772c021a0da04b4d33923ddebd9014719f5a6cfb
31Carlton7 <carltonaikins7@gmail.com>
2021-09-03 12:43:59 -0400

    Remove print statement & Update imports

--- lib/src/ui/views/chat_view/add_users_to_chat_view.dart
+++ lib/src/ui/views/chat_view/add_users_to_chat_view.dart
@@ -2,7 +2,7 @@ import 'package:canton_design_system/canton_design_system.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kounslr/src/models/student.dart';
-import 'package:kounslr/src/ui/providers/all_students_stream_provider.dart';
+import 'package:kounslr/src/providers/all_students_stream_provider.dart';
 import 'package:kounslr/src/ui/styled_components/something_went_wrong.dart';
 import 'package:kounslr/src/ui/views/chat_view/components/add_student_card.dart';
 import 'package:kounslr/src/ui/views/chat_view/components/add_students_search_bar.dart';
@@ -43,7 +43,6 @@ class _AddUsersToChatViewState extends State<AddUsersToChatView> {
         final studentsRepo = watch(allStudentsStreamProvider);
         return studentsRepo.when(
           error: (e, s) {
-            print(e);
             return Expanded(child: SomethingWentWrong());
           },
           loading: () => Expanded(child: Loading()),
