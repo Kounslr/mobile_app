cd2eed148e2d97b551e6439a8cfcfd61f19be075
31Carlton7 <carltonaikins7@gmail.com>
2021-09-03 12:43:08 -0400

    Update imports

--- lib/src/ui/styled_components/sign_out_button.dart
+++ lib/src/ui/styled_components/sign_out_button.dart
@@ -1,6 +1,6 @@
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kounslr/src/ui/providers/authentication_providers/authentication_service_provider.dart';
+import 'package:kounslr/src/providers/authentication_providers/authentication_service_provider.dart';
 
 class SignOutButton extends StatelessWidget {
   const SignOutButton();
