2836cc5db7bd80f79d002ac0b2c11a9b9120bb19
Carlton Aikins <carltonaikins7@gmail.com>
2021-11-20 19:05:11 -0500

    Execute license header script

--- lib/src/services/repositories/school_repository.dart
+++ lib/src/services/repositories/school_repository.dart
@@ -1,3 +1,21 @@
+/*
+Kounslr iOS & Android App
+Copyright (C) 2021 Kounslr
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:kounslr/src/models/assignment.dart';
 import 'package:kounslr/src/models/block.dart';
