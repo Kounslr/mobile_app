18c5fe5193f6af97c02a62154cbf311d618fc1d9
Carlton Aikins <carltonaikins7@gmail.com>
2021-11-25 17:14:59 -0500

    Relocate files

---
+++ lib/src/providers/student_classes_stream_provider.dart
@@ -0,0 +1,34 @@
+/*
+Kounslr iOS & Android App
+Copyright (C) 2021 Kounslr
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kounslr/src/models/class.dart';
+import 'package:kounslr/src/services/repositories/student_repository.dart';
+
+import 'authentication_providers/authentication_stream_provider.dart';
+
+final studentClassesStreamProvider = StreamProvider.autoDispose<List<Class>>((ref) {
+  ref.watch(authenticationStreamProvider).whenData((value) {
+    if (value == null) {
+      ref.maintainState = false;
+    } else {
+      ref.maintainState = true;
+    }
+  });
+  return StudentRepository().studentClasses;
+});
