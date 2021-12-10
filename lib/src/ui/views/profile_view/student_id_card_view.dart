ed952032a4d661f18a86533021cb448a31272bd4
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-10 15:48:02 -0500

    Update ../lib/src/ui/views/profile_view/student_id_card_view.dart

--- lib/src/ui/views/profile_view/student_id_card_view.dart
+++ lib/src/ui/views/profile_view/student_id_card_view.dart
@@ -21,7 +21,6 @@ import 'package:kounslr/src/models/school.dart';
 import 'package:kounslr/src/models/student.dart';
 import 'package:kounslr/src/ui/components/student_id_card.dart';
 import 'package:kounslr/src/ui/views/profile_view/components/student_id_card_view_header.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
 class StudentIDCardView extends StatelessWidget {
   final Student student;
@@ -30,19 +29,17 @@ class StudentIDCardView extends StatelessWidget {
   const StudentIDCardView(this.student, this.school, {Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return _content(context);
+    return CantonScaffold(backgroundColor: CantonMethods.alternateCanvasColor(context), body: _content(context));
   }
 
   Widget _content(BuildContext context) {
-    return CantonScaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const StudentIdCardViewHeader(),
-          _body(context, student),
-          Container(height: 70),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const StudentIdCardViewHeader(),
+        _body(context, student),
+        Container(height: 70),
+      ],
     );
   }
 
@@ -50,13 +47,15 @@ class StudentIDCardView extends StatelessWidget {
     return Column(
       children: [
         StudentIDCard(student, school),
-        const SizedBox(height: 80),
-        QrImage(
-          data: 'https://www.kounslr.com',
-          version: QrVersions.auto,
-          backgroundColor: CantonColors.bgPrimary,
-          size: 100,
-        ),
+        // const SizedBox(height: 80),
+
+        /// Implement Custom API with student Auth verification & QR Code capabilities
+        // QrImage(
+        //   data: 'https://www.kounslr.com',
+        //   version: QrVersions.auto,
+        //   backgroundColor: CantonColors.bgPrimary,
+        //   size: 100,
+        // ),
       ],
     );
   }
