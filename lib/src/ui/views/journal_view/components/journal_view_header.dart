47fdc82622a168dff234c2bb67700202d20c5ea3
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-11 15:02:54 -0500

    Update ../lib/src/ui/views/journal_view/components/journal_view_header.dart

--- lib/src/ui/views/journal_view/components/journal_view_header.dart
+++ lib/src/ui/views/journal_view/components/journal_view_header.dart
@@ -17,6 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:canton_design_system/canton_design_system.dart';
+
 import 'package:kounslr/src/models/journal_entry.dart';
 import 'package:kounslr/src/ui/views/journal_entry_view/journal_entry_view.dart';
 
