0f295237d3348325614dec19783b841186492049
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-11 15:01:52 -0500

    Update ../lib/src/ui/views/journal_entries_view/components/show_tag_rename_bottom_sheet.dart

--- lib/src/ui/views/journal_entries_view/components/show_tag_rename_bottom_sheet.dart
+++ lib/src/ui/views/journal_entries_view/components/show_tag_rename_bottom_sheet.dart
@@ -1,6 +1,8 @@
-import 'package:canton_design_system/canton_design_system.dart';
 import 'package:flutter/services.dart';
+
+import 'package:canton_design_system/canton_design_system.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:kounslr/src/models/journal_entry.dart';
 import 'package:kounslr/src/providers/student_repository_provider.dart';
 
